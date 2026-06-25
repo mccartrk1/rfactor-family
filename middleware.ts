2a95be861425264ee87b5642d081a4cc896fbcc0
mccartrk1 <mccartrk@gmail.com>
2026-06-25 09:58:03 -0400

    Temp: disable auth protection

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 export { default } from 'next-auth/middleware'
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/onboard/:path*', '/lesson/:path*', '/journey/:path*'],
+  matcher: [],
 }
