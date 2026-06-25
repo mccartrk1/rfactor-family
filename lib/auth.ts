b4a5c828ca4e021d0201539744f419c3621c52b6
mccartrk1 <mccartrk@gmail.com>
2026-06-24 22:59:09 -0400

    test: JWT sessions to isolate OAuth error

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,33 +1,33 @@
 // lib/auth.ts
 import { NextAuthOptions } from 'next-auth'
-import { PrismaAdapter } from '@auth/prisma-adapter'
 import GoogleProvider from 'next-auth/providers/google'
-import { db } from './db'
 
 export const authOptions: NextAuthOptions = {
-  debug: true,
-  adapter: PrismaAdapter(db),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  callbacks: {
-    async session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id
-      }
-      return session
-    },
+  session: {
+    strategy: 'jwt',
   },
   pages: {
     signIn: '/auth/login',
     error: '/auth/login',
   },
-  session: {
-    strategy: 'database',
-    maxAge: 7 * 24 * 60 * 60,
-    updateAge: 24 * 60 * 60,
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id
+      }
+      return token
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string
+      }
+      return session
+    },
   },
 }
